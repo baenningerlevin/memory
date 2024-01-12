fa10a63083b53c9528ac53133227171a3e791b9d
Levin Baenninger <levin.baenninger@buhlergroup.com>
2024-01-12 16:13:39 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const cardContent = [
+  '⚽',
+  '⚽',
+  '⚾',
+  '⚾',
+  '🏀',
+  '🏀',
+  '🏈',
+  '🏈',
+  '🎱',
+  '🎱',
+  '🏒',
+  '🏒',
+  '🥋',
+  '🥋',
+  '🏓',
+  '🏓',
+];
+const shuffledCards = cardContent.sort(() => Math.random() - 0.5);
+const game = document.querySelector('.game');
+const resetBtn = document.querySelector('.reset');
+
+for (let i = 0; i < cardContent.length; i++) {
+  // Create cards
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.textContent = shuffledCards[i];
+  game.appendChild(card);
+
+  // Flip cards and check for match
+  card.addEventListener('click', () => {
+    card.classList.add('flip');
+    setTimeout(() => checkForMatch(), 2000);
+  });
+}
+
+resetBtn.addEventListener('click', () => {
+  location.reload();
+});
+
+const checkForMatch = () => {
+  const flippedCards = document.querySelectorAll('.flip');
+
+  if (flippedCards.length > 1) {
+    if (flippedCards[0].textContent === flippedCards[1].textContent) {
+      flippedCards[0].classList.add('match');
+      flippedCards[1].classList.add('match');
+
+      flippedCards[1].classList.remove('flip');
+      flippedCards[0].classList.remove('flip');
+
+      if (document.querySelectorAll('.match').length === cardContent.length) {
+        alert('You won!');
+      }
+    } else {
+      flippedCards[1].classList.remove('flip');
+      flippedCards[0].classList.remove('flip');
+    }
+  }
+};
